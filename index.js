97a4c21ebe01d5f735c61184392c0fa714b1c34f
apikmeister <afiq.mohamad90@gmail.com>
2022-07-05 18:33:41 +0800

    cleanup

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
-const api = express();
-const PORT = 8080;
-const request = require("request-promise");
-const cheerio = require("cheerio");
-const mongoose= require('mongoose');
-const bodyparser=require('body-parser');
-const cookieParser=require('cookie-parser');
-const { response } = require('express');
+const request = require('request');
+const cheerio = require('cheerio');
+const axios = require('axios');
+const app = express();
+const PORT = process.env.PORT || 8008;
+// const request = require("request-promise");
+// const mongoose= require('mongoose');
+// const bodyparser=require('body-parser');
+// const cookieParser=require('cookie-parser');
+// const { response } = require('express');
 
 request("https://epembelajaran.umt.edu.my/oceania/auth/saml/index.php", (error, response, html) => {
     if (!error && response.statusCode == 200) {
@@ -14,36 +16,47 @@ request("https://epembelajaran.umt.edu.my/oceania/auth/saml/index.php", (error, 
     }
 })
 
-api.use(express.json())
-api.use(bodyparser.urlencoded({extended : false}));
-api.use(bodyparser.json);
-api.use(cookieParser());
-
-api.listen(
+app.listen(
     PORT,
     () => console.log(`it's alive on http://localhost:${PORT}`)
 );
 
+app.get('/', function(req, res) {
+    let result = req.query.result;
+
+    let url = 'https://pelajar.mynemo.umt.edu.my/index.php?module=hepa&app=mystar&page=trans';
 
 
-//run function after get
-api.get('/tshirt', (req, res)/*request(incoming) and response(outgoing)*/ => {
-    res.status(200).send({/*data payload*/
-        tshirt : 'wow',
-        size : 'medium'
-    })
 });
 
-//dynamic url param
-api.post('/tshirt/:id'/*capture dynamic value in URL*/, (req, res) => {
-    const { id } = req.params;
-    const { logo } = req.body;
+// request(url, function(error, response, html) {
+//     if(!error) {
+//         let $ = cheerio.load(html);
+//     }
+// });
 
-    if (!logo) {
-        res.status(418).send({ message: 'We need a logo!' })
-    }
+// let mystar = $(span.style80 > strong);
 
-    res.send({
-        tshirt: `Shirt with your ${logo} and ID of ${id}`,
-    });
-});
+// module.exports = api;
+
+// //run function after get
+// api.get('/tshirt', (req, res)/*request(incoming) and response(outgoing)*/ => {
+//     res.status(200).send({/*data payload*/
+//         tshirt : 'wow',
+//         size : 'medium'
+//     })
+// });
+
+// //dynamic url param
+// api.post('/tshirt/:id'/*capture dynamic value in URL*/, (req, res) => {
+//     const { id } = req.params;
+//     const { logo } = req.body;
+
+//     if (!logo) {
+//         res.status(418).send({ message: 'We need a logo!' })
+//     }
+
+//     res.send({
+//         tshirt: `Shirt with your ${logo} and ID of ${id}`,
+//     });
+// });
