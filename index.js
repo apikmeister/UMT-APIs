c98d5b7dbdd25e96fb424de8e53331a56e8e6719
apikmeister <afiq.mohamad90@gmail.com>
2022-07-09 10:10:35 +0800

    base

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const express = require('express');
 const request = require('request');
 const cheerio = require('cheerio');
 const axios = require('axios');
+const bodyparser = require('body-parser');
+const bodyParser = require('body-parser');
+const api = require(`./api/api`);
+
 const app = express();
+
 const PORT = process.env.PORT || 8008;
 // const request = require("request-promise");
 // const mongoose= require('mongoose');
@@ -10,24 +15,44 @@ const PORT = process.env.PORT || 8008;
 // const cookieParser=require('cookie-parser');
 // const { response } = require('express');
 
-request("https://epembelajaran.umt.edu.my/oceania/auth/saml/index.php", (error, response, html) => {
-    if (!error && response.statusCode == 200) {
-        const $= cheerio.load(html);
-    }
-})
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+// request("https://epembelajaran.umt.edu.my/oceania/auth/saml/index.php", (error, response, html) => {
+//     if (!error && response.statusCode == 200) {
+//         const $= cheerio.load(html);
+//     }
+// })
+
+app.get('/', function(req, res) {
+    res.send(`Welcome to UMT-apis.`)
+    // let result = req.query.result;
+
+    // let url = 'https://pelajar.mynemo.umt.edu.my/index.php?module=hepa&app=mystar&page=trans';
+});
+
+app.use(`/api`, api);
+
+//Handle 404
+app.use(function(req, res) {
+    res.status(400).json({
+        error:"Page not found!"
+    });
+});
+
+
+//Handle 500
+app.use(function(error, req, res, next) {
+    res.status(500).json({
+        error:error
+    });
+});
+
 
 app.listen(
     PORT,
     () => console.log(`it's alive on http://localhost:${PORT}`)
 );
-
-app.get('/', function(req, res) {
-    let result = req.query.result;
-
-    let url = 'https://pelajar.mynemo.umt.edu.my/index.php?module=hepa&app=mystar&page=trans';
-
-
-});
 
 // request(url, function(error, response, html) {
 //     if(!error) {
